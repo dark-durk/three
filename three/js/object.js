d2873ff3baf3cd0042d89a8b15b9970d7572cf9a
dark-durk <210009804@qq.com>
2021-09-18 16:19:50 +0800

    02

--- three/js/object.js
+++ three/js/object.js
@@ -21,53 +21,50 @@ Stairs.prototype.createStairsVertices = function () {
 
   vertices.push(new THREE.Vector2(this.num * this.width, this.num * this.height - 200))
   vertices.unshift(new THREE.Vector2(0, -200))
-  // console.log(vertices)
   return vertices
 }
 
-export function Bench(h = 180, fh = 180, fw = 500, uw = 500, uh = 180) {
+export function  Flight(h = 180, upThickness = 180, upLevelWidth = 500, downLevelWidth = 500, downThickness = 180) {
   this.h = h
-  this.fh = fh
-  this.fw = fw
-  this.uh = uh
-  this.uw = uw
+  this.upThickness = upThickness
+  this.upLevelWidth = upLevelWidth
+  this.downThickness = downThickness
+  this.downLevelWidth = downLevelWidth
 }
-Bench.prototype.createBenchVertices = function (stairs) {
-  let vertices = [], upVertice = [], underVertice = []
+ Flight.prototype.createFlightVertices = function (stairs) {
+  let vertices = [], upVertice = [], downVertice = []
   let k = stairs.height / stairs.width // 161/260
   // y=k*x-h
-  underVertice.push(new THREE.Vector2((-this.uh + this.h) / k, -this.uh))
-  underVertice.push(new THREE.Vector2(-this.uw, -this.uh), new THREE.Vector2(-this.uw, 0))
+  downVertice.push(new THREE.Vector2((-this.downThickness + this.h) / k, -this.downThickness))
+  downVertice.push(new THREE.Vector2(-this.downLevelWidth, -this.downThickness), new THREE.Vector2(-this.downLevelWidth, 0))
 
-  upVertice.push(new THREE.Vector2(stairs.totalWidth, stairs.totalHeight + this.fh), new THREE.Vector2(stairs.totalWidth + this.fw, stairs.totalHeight + this.fh))
-  upVertice.push(new THREE.Vector2(stairs.totalWidth + this.fw, stairs.totalHeight))
+  upVertice.push(new THREE.Vector2(stairs.totalWidth, stairs.totalHeight + this.upThickness), new THREE.Vector2(stairs.totalWidth + this.upLevelWidth, stairs.totalHeight + this.upThickness))
+  upVertice.push(new THREE.Vector2(stairs.totalWidth + this.upLevelWidth, stairs.totalHeight))
   upVertice.push(new THREE.Vector2((stairs.totalHeight + this.h) / k, stairs.totalHeight))
-  vertices.push(underVertice, upVertice)
-  // console.log(vertices)
+  vertices.push(downVertice, upVertice)
   return vertices
 }
 
-export function LadderBeam(beamWidth = 200, beamHeight = 400, tiaoerWidth = 200, tiaoerHeight = 200, sinkHeight = 200) {
+export function LadderBeam(beamWidth = 200, beamHeight = 400, corbelWidth = 200, corbelHeight = 200, sinkHeight = 200) {
   this.beamWidth = beamWidth
   this.beamHeight =beamHeight
-  this.tiaoerWidth = tiaoerWidth
-  this.tiaoerHeight = tiaoerHeight
+  this.corbelWidth = corbelWidth
+  this.corbelHeight = corbelHeight
   this.sinkHeight = sinkHeight
 }
-LadderBeam.prototype.createLadderBeamVertices = function (stairs, bench, feng) {
+LadderBeam.prototype.createLadderBeamVertices = function (stairs, flight, seam) {
   let vertices = []
-  vertices.push(new THREE.Vector2(stairs.totalWidth + bench.fw + feng.f5, stairs.totalHeight + bench.fh))
-  vertices.push(new THREE.Vector2(stairs.totalWidth + bench.fw + feng.f5 + this.beamWidth, stairs.totalHeight + bench.fh))
-  vertices.push(new THREE.Vector2(stairs.totalWidth + bench.fw + feng.f5 + this.beamWidth, stairs.totalHeight + bench.fh - this.beamHeight))
-  vertices.push(new THREE.Vector2(stairs.totalWidth + bench.fw + feng.f5, stairs.totalHeight + bench.fh - this.beamHeight))
-  vertices.push(new THREE.Vector2(stairs.totalWidth + bench.fw + feng.f5 - this.tiaoerWidth, stairs.totalHeight + bench.fh - this.beamHeight))
-  vertices.push(new THREE.Vector2(stairs.totalWidth + bench.fw + feng.f5 - this.tiaoerWidth, stairs.totalHeight + bench.fh - this.beamHeight + this.tiaoerHeight))
-  vertices.push(new THREE.Vector2(stairs.totalWidth + bench.fw + feng.f5, stairs.totalHeight + bench.fh - this.beamHeight + this.tiaoerHeight))
-  // console.log(vertices)
+  vertices.push(new THREE.Vector2(stairs.totalWidth + flight.upLevelWidth + seam.f5, stairs.totalHeight + this.beamHeight-this.sinkHeight))
+  vertices.push(new THREE.Vector2(stairs.totalWidth + flight.upLevelWidth + seam.f5 + this.beamWidth, stairs.totalHeight + this.beamHeight-this.sinkHeight))
+  vertices.push(new THREE.Vector2(stairs.totalWidth + flight.upLevelWidth + seam.f5 + this.beamWidth, stairs.totalHeight + this.beamHeight-this.sinkHeight - this.beamHeight))
+  vertices.push(new THREE.Vector2(stairs.totalWidth + flight.upLevelWidth + seam.f5, stairs.totalHeight + this.beamHeight-this.sinkHeight - this.beamHeight))
+  vertices.push(new THREE.Vector2(stairs.totalWidth + flight.upLevelWidth + seam.f5 - this.corbelWidth, stairs.totalHeight + this.beamHeight-this.sinkHeight - this.beamHeight))
+  vertices.push(new THREE.Vector2(stairs.totalWidth + flight.upLevelWidth + seam.f5 - this.corbelWidth, stairs.totalHeight + this.beamHeight-this.sinkHeight - this.beamHeight + this.corbelHeight))
+  vertices.push(new THREE.Vector2(stairs.totalWidth + flight.upLevelWidth + seam.f5, stairs.totalHeight + this.beamHeight-this.sinkHeight - this.beamHeight + this.corbelHeight))
   return vertices
 }
 
-export function Feng(f1 = 20, f2 = 20, f3 = 20, f4 = 20, f5 = 30, f6 = 20) {
+export function Seam(f1 = 20, f2 = 20, f3 = 20, f4 = 20, f5 = 30, f6 = 20) {
   this.f1 = f1
   this.f2 = f2
   this.f3 = f3
